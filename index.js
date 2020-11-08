d1e9ccb7f36d58203aeade3e525be1b808621f65
Kaueny de Oliveira Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2020-11-07 21:24:32 -0300

    Lógicas e Layout Criados

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import cipher from './cipher.js';
 
+const buttonCrip= document.getElementById("codificar")
+buttonCrip.addEventListener("click", crip)
+
+function crip(event) {
+  event.preventDefault()
+	let stringCrip = document.getElementById("stringCrip").value;
+  let offsetCrip = parseInt(document.getElementById("offsetCrip").value);
+  document.getElementById("mensagemCodificada").value = cipher.encode(offsetCrip, stringCrip);
+}
 
 
-console.log(cipher)
+const buttonDescrip= document.getElementById("decodificar")
+buttonDescrip.addEventListener("click", descrip)
 
-
-
-
+function descrip(event) {
+    event.preventDefault()
+	let stringDescrip = document.getElementById("stringDescrip").value;
+  let offsetDescrip = parseInt(document.getElementById("offsetDescrip").value);
+  document.getElementById("mensagemDecodificada").value = cipher.decode(offsetDescrip, stringDescrip);
+}
 
