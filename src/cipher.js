ecbddb404b853269c4f9badbc2602aaa11d024e0
Kaueny de Oliveira Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2020-11-07 21:24:32 -0300

    Lógicas e Layout Criados

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,53 @@
 
-import { encode } from "./encode.js";
-import { decode } from "./decode.js";
+ const cipher = {
 
-const cipher = {
+ encode(offset, string) {
 
-  encode,
-  decode
+    if(offset === null || string === [] && offset === 0 || string === 0){
+      throw new TypeError()
+  }
+
+    let tamAlfabeto = 26
+    let posicao1Letra = 65
+    let posicaoCodificada;
+    let palavraConcatenada ="";
+
+    for (let i = 0; i < string.length; i++) {
+
+        posicaoCodificada = (((string.charCodeAt(i)) - posicao1Letra + offset ) % tamAlfabeto) + posicao1Letra
+        palavraConcatenada += String.fromCharCode(posicaoCodificada);
+    }
+
+    return palavraConcatenada
+
+    },
+
+     decode(offset, string) {
+
+      if(offset === null || string === [] && offset === 0 || string === 0){
+            throw new TypeError()
+        }
+
+      let tamAlfabeto = 26
+      let posicao1Letra = 65
+      let posicaoDecodificada;
+      let palavraConcatenada ="";
+
+      for (let i = 0; i < string.length; i++) {
+
+          posicaoDecodificada = ((string.charCodeAt(i) + posicao1Letra - offset) % tamAlfabeto) + posicao1Letra
+          palavraConcatenada += String.fromCharCode(posicaoDecodificada);
+      }
+
+      return palavraConcatenada
+    },
 
 };
 
 
-export default cipher;
+
+
+
+ export default cipher;
 
 
